add68380b40dcaf15e41d463e2f70c169164c129
Adrian Dalca <adalca@mit.edu>
2014-06-19 19:07:41 -0400

    cleaning up examples

--- examples/example_grid.m
+++ examples/example_grid.m
@@ -4,9 +4,9 @@ function example_grid()
     r = rand(11, 11);
     p = [5, 5];
     idx = patchlib.grid(size(r), p, -1, 'sub');
-    pv.patchesInImage(r, [idx{1}(:), idx{2}(:)], p, 'top-left')
+    pv.patchesInImage(r, p, [idx{1}(:), idx{2}(:)], 'top-left')
     
     r = rand(8, 8);
     idx = patchlib.grid(size(r), p, 2, 'sub');
-    pv.patchesInImage(r, [idx{1}(:), idx{2}(:)], p, 'top-left')
+    pv.patchesInImage(r, p, [idx{1}(:), idx{2}(:)], 'top-left')
 
