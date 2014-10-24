36a8a368b2d165b564235fd74403e774bf10809a
Adrian Dalca <adalca@mit.edu>
2014-10-24 16:36:56 -0400

    adding example for grid2D

---
+++ examples/example_grid2D.m
@@ -0,0 +1,10 @@
+function example_grid2D()
+    sx = 30;
+    sy = 20;
+    
+    v = rand(sx, sy);
+    H = fspecial('gaussian',10,10);
+    v = imfilter(v,H,'replicate');
+    
+    idx = patchlib.grid([sx, sy], [5, 5], 'mrf');
+    patchview.grid2D(idx, v);
