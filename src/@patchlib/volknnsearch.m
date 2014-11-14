38c76445af5db8aab2926c7fa81e4eff37ffc368
Adrian Dalca <adalca@mit.edu>
2014-11-14 16:32:57 -0500

    added global/local refines for local volknnsearch

--- src/@patchlib/volknnsearch.m
+++ src/@patchlib/volknnsearch.m
@@ -122,41 +122,100 @@ function [pIdx, pRefIdxs, pDst] = localsearch(src, refs, spacing, varargin)
     maxgridsub = cellfun(fn, refs.gridSize, 'UniformOutput', false);
     
     % get input K
-    f = find(strcmp('K', varargin), 1, 'first');
-    K = ifelse(isempty(f), '1', 'varargin{f + 1}', true);
+    fK = find(strcmp('K', varargin), 1, 'first');
+    K = ifelse(isempty(fK), '1', 'varargin{fK + 1}', true);
         
     % go through each voxel, get reference patches from nearby from each reference.
     pIdx = nan(size(src.lib, 1), K);
     pRefIdxs = nan(size(src.lib, 1), K);
     pDst = nan(size(src.lib, 1), K);
     subset = find(src.mask(src.grididx))';
-    for i = subset %1:size(src.lib, 1)
-        
-        % compute the reference linear idx and reference number for the regions around this voxel
-        ridxwindow = cell(nRefs, 1);
-        ipatches = cell(nRefs, 1);
-        refIdx = cell(nRefs, 1);
-        for r = 1:nRefs
-            idxsel = bsxfun(@ge, refs.subs{r}, mingridsub(i, :)) & ...
-                bsxfun(@le, refs.subs{r}, maxgridsub{r}(i, :));
-            ridxwindow{r} = ridx{r}(all(idxsel, 2));
-            ridxwindow{r} = ridxwindow{r}(:);    
-            
-            ipatches{r} = refs.lib{r}(ridxwindow{r}, :);
-            refIdx{r} = r * ones(size(ridxwindow{r}));
-        end
-        gidxsall = cat(1, ridxwindow{:});        
-        ipatchesall = cat(1, ipatches{:});
-        riall = cat(1, refIdx{:});
-        assert(size(gidxsall, 1) >= K, 'Spacing does not allow %d nearest neighbours', K);
-        
-        [p, d] = knnsearch(ipatchesall, src.lib(i, :), varargin{:});
-        
-        pIdx(i, :) = gidxsall(p);
-        pDst(i, :) = d; 
-        pRefIdxs(i, :) = riall(p);
+    
+    % if there is a user specified distance, do local refine.
+    % otherwise, globalrefine is actually probably faster due to the mex implementation.
+    f = find(strcmp('Distance', varargin));
+    if numel(f) > 0 && ~ischar(varargin{f+1})
+        method = 'localrefine'; 
+    else
+        method = 'globalrefine';
     end
+    method 
+    switch method
+        case 'localrefine'
+
+            for i = subset %1:size(src.lib, 1)
+                % compute the reference linear idx and reference number for the regions around this voxel
+                ridxwindow = cell(nRefs, 1);
+                ipatches = cell(nRefs, 1);
+                refIdx = cell(nRefs, 1);
+                for r = 1:nRefs
+                    idxsel = bsxfun(@ge, refs.subs{r}, mingridsub(i, :)) & ...
+                        bsxfun(@le, refs.subs{r}, maxgridsub{r}(i, :));
+                    ridxwindow{r} = ridx{r}(all(idxsel, 2));
+                    ridxwindow{r} = ridxwindow{r}(:);    
+
+                    rlib = refs.lib{r};
+                    ipatches{r} = rlib(ridxwindow{r}, :);
+                    refIdx{r} = r * ones(size(ridxwindow{r}));
+                end
+                gidxsall = cat(1, ridxwindow{:});        
+                ipatchesall = cat(1, ipatches{:});
+                riall = cat(1, refIdx{:});
+                assert(size(gidxsall, 1) >= K, 'Spacing does not allow %d nearest neighbours', K);
+
+                [p, d] = knnsearch(ipatchesall, src.lib(i, :), varargin{:});
+
+                pIdx(i, :) = gidxsall(p);
+                pDst(i, :) = d; 
+                pRefIdxs(i, :) = riall(p);
+            end
+            
+        case 'globalrefine' % first computes *all* the distances.
+            
+            if numel(fK) == 1
+                varargin(fK:fK+1) = [];
+            end
+            
+            tmp_d = pdist2(src.lib, cat(1, refs.lib{:}), varargin{:});
+            %     tmp_d = pdist2mex(src.lib',cat(1, refs.lib{:})','euc',[],[],[]);
+            for i = subset 
+                
+                % compute the reference linear idx and reference number for the regions around this voxel
+                ridxwindow = cell(nRefs, 1);
+                refIdx = cell(nRefs, 1);
+                for r = 1:nRefs
+                    idxsel = bsxfun(@ge, refs.subs{r}, mingridsub(i, :)) & ...
+                        bsxfun(@le, refs.subs{r}, maxgridsub{r}(i, :));
+                    ridxwindow{r} = ridx{r}(all(idxsel, 2));
+                    ridxwindow{r} = ridxwindow{r}(:);
+                    refIdx{r} = r * ones(size(ridxwindow{r}));
+                end
+                gidxsall = cat(1, ridxwindow{:});
+                riall = cat(1, refIdx{:});
+                assert(size(gidxsall, 1) >= K, 'Spacing does not allow %d nearest neighbours', K);
+                
+                tmp_gidxsall = pr2idx(gidxsall, riall, cellfun(@(x) size(x, 1), refs.lib));
+                [tmp_d2, tmp_di] = sort(tmp_d(i, tmp_gidxsall), 'ascend');
+                pIdx(i, :) = gidxsall(tmp_di(1:K))';
+                pDst(i, :) = tmp_d2(1:K);
+                pRefIdxs(i, :) = riall(tmp_di(1:K))';
+            end
+            
+        otherwise
+            error('Internal method not found. Blame Adrian');
+    end
+    
+    
+    
 end
+
+function idx = pr2idx(pIdx, rIdx, refSizes)
+    idx = pIdx * 0;
+    for i = 1:numel(refSizes)
+        idx(rIdx == i) = pIdx(rIdx == i) + sum(refSizes(1:i-1));
+    end 
+end
+
 
 function [pIdx, pRefIdxs, pDst] = globalsearch(src, refs, varargin)
 % do a global search
