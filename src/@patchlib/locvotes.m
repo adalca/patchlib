f13af89bf5c8e98252284e295d1de54f91d1f9c8
Adrian Dalca <adalca@mit.edu>
2014-11-29 20:40:51 -0500

    updated locvotes to make it more flexible.

--- src/@patchlib/locvotes.m
+++ src/@patchlib/locvotes.m
@@ -1,10 +1,12 @@
-function [votes, pIdx, locIdx, idx] = locvotes(loc, volSize, patches, grididx, patchSize)
+function [votes, pIdx, locIdx, idx] = locvotes(loc, patches, grididx, patchSize, volSize)
 % VOTEIDX votes given volume location
-%   votes = locvotes(loc, volSize, patches, grididx, patchSize) patch votes for a given volume
+%   votes = locvotes(loc, patches, grididx, patchSize, volSize) patch votes for a given volume
 %   location loc inside a volume of size volSize, patches are the resulting patches from a knnsearch
 %   sized (N x P x K), grididx is the patches grid. patchSize is the patch size.
 %
-%   [votes, pIdx, locIdx, idx] = locvotes(loc, volSize, patches, grididx, patchSize) also allows for
+%   votes = locvotes(loc, patches, gridloc, patchSize) % allows for passing the gridloc directly.
+%
+%   [votes, pIdx, locIdx, idx] = locvotes(...) also allows for
 %   the return of pIdx (the index of which patches are returned, i.e. index in rows of 'patches'),
 %   and locIdx is the index of the locations used for those patches. Note that all K votes are
 %   returned for each of these. idx is the final index computed from pIdx and locIdx and considering
@@ -15,13 +17,19 @@ function [votes, pIdx, locIdx, idx] = locvotes(loc, volSize, patches, grididx, p
 %   Author: adalca@csail.mit.edu   
     
     % input checking
-    narginchk(5, 5);
-    grididx = grididx(:);
-    assert(numel(grididx) == size(patches, 1), 'grididx and patches are not compatible');
+    narginchk(4, 5);
+    if nargin == 5
+        grididx = grididx(:);
+        assert(numel(grididx) == size(patches, 1), 'grididx and patches are not compatible');
+        gridloc = ind2subvec(volSize, grididx);
+    else
+        gridloc = grididx;
+        assert(size(gridloc, 1) == size(patches, 1), 'gridloc and patches are not compatible');
+    end
     assert(prod(patchSize) == size(patches, 2), 'patchSize and patches are not compatible');
 
     % retreive vote indexes
-    [pIdx, locIdx] = voteidx(loc, volSize, grididx, patchSize);
+    [pIdx, locIdx] = voteidx(loc, gridloc, patchSize);
     assert(isempty(pIdx) || max(pIdx) <= size(patches, 1), ...
         'pIdx failure: %d, %d', max(pIdx), size(patches, 1));
     
@@ -39,17 +47,15 @@ function [votes, pIdx, locIdx, idx] = locvotes(loc, volSize, patches, grididx, p
     votes = patches(idx);
 end
 
-function [pIdx, locIdx] = voteidx(loc, volSize, grididx, patchSize)
+function [pIdx, locIdx] = voteidx(loc, gridloc, patchSize)
 % VOTEIDX get the voting patches and the location inside those patches for a given volume location
-%   [pIdx, locIdx] = voteidx(loc, volSize, grididx, patchSize) get the voting patches and the
-%   location inside those patches for a given volume location. loc - the location inside
-%   a volume of size volSize. grididx is the patches grid. patchSize is the patch size.
+%   [pIdx, locIdx] = voteidx(loc, gridloc, patchSize) get the voting patches and the location inside
+%   those patches for a given volume location. loc - the location inside a volume of size volSize.
+%   gridloc is the locations of the patches grid. patchSize is the patch size.
 %
 %   Author: adalca@csail.mit.edu
     
     assert(numel(loc) == numel(patchSize));
-    
-    gridloc = ind2subvec(volSize, grididx);
     
     sel = bsxfun(@ge, gridloc, (loc - patchSize + 1));
     sel = sel & bsxfun(@le, gridloc, loc);
--- src/@patchlib/patchlib.m
+++ src/@patchlib/patchlib.m
@@ -52,7 +52,7 @@ classdef patchlib < handle
         rect = drawPatchRect(patchloc, patchSize, color);
         isv = isvalidoverlap(overlap);
         [sub, loc, corresp] = corresp2disp(siz, varargin);
-        [votes, pIdx, locIdx, idx] = locvotes(loc, volSize, patches, grididx, patchSize);
+        [votes, pIdx, locIdx, idx] = locvotes(loc, patches, grididx, patchSize, volSize);
     end
     
     methods (Static, Access = private)
