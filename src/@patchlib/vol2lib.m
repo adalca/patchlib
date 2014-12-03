8f177f650ed3efa067dffd816d8b6a9915f99edc
Adrian Dalca <adalca@mit.edu>
2014-12-03 13:39:23 -0500

    adding partial-write support for vol2lib

--- src/@patchlib/vol2lib.m
+++ src/@patchlib/vol2lib.m
@@ -1,31 +1,38 @@
 function varargout = vol2lib(vol, patchSize, varargin)
 % VOL2LIB transform a volume into a patch library
 %   library = vol2lib(vol, patchSize) transform volume vol to a patch
-%       library. vol can be any dimensions (tested for 2, 3); patchSize is the size
-%       of the patch (nDims x 1 vector). The default assumption is that the patches overlap
-%       by (patchSize - 1) (i.e. 'sliding' patches). See below for options on changing this. 
-%       The size of the library is then the number of patches that fit in the image x number
-%       of voxels in the patch.
+%   library. vol can be any dimensions (tested for 2, 3); patchSize is the size of the patch
+%   (nDims x 1 vector). The default assumption is that the patches overlap by (patchSize - 1)
+%   (i.e. 'sliding' patches). See below for options on changing this. The size of the library is
+%   then the number of patches that fit in the image x number of voxels in the patch.
 %
-%       Alternatively, vol can be a cell array of volumes, in which case the library is computed for
-%       each volumes. library is then a cell array with as many entries as volumes.
+%   Alternatively, vol can be a cell array of volumes, in which case the library is computed for
+%   each volumes. library is then a cell array with as many entries as volumes.
 %
 %   library = vol2lib(vol, patchSize, overlap) allow specification of how the overlap between
-%       patches: a scalar, vector (of size [1xnDims]) or a string for a pre-specified configuration,
-%       like 'sliding', 'discrete', or 'mrf'. see patchlib.overlapkind for details of the supported
-%       overlap kinds. If not specified (i.e. function has only 2 inputs), default overlap is
-%       'sliding'.
+%   patches: a scalar, vector (of size [1xnDims]) or a string for a pre-specified configuration,
+%   like 'sliding', 'discrete', or 'mrf'. see patchlib.overlapkind for details of the supported
+%   overlap kinds. If not specified (i.e. function has only 2 inputs), default overlap is
+%   'sliding'.
 %
 %   Note: vol2lib will cut the volume to fit the right number of patches.
 %
 %   [library, idx, libVolSize, gridSize] = vol2lib(...) returns the index of the starting (top-left)
-%       point of every patch in the *original* volume, and the size of the volumes size, which is
-%       smaller than or equal to the size of vol. It will be smaller than the initial volume if the
-%       volume had to be cropped. Also returns the number of patches gridSize.
+%   point of every patch in the *original* volume, and the size of the volumes size, which is
+%   smaller than or equal to the size of vol. It will be smaller than the initial volume if the
+%   volume had to be cropped. Also returns the number of patches gridSize.
 %
 %   [..., libIdx] = vol2lib(...) only given a cell of vols, returns a cell array with each entry
-%       being a column vector with the same number of rows as library, indicating the volume index
-%       (i.e. 1..numel(vols))
+%   being a column vector with the same number of rows as library, indicating the volume index (i.e.
+%   1..numel(vols))
+%
+%   [libfile, idx, libVolSize, gridSize] = vol2lib(..., 'savefile', 'filename.mat') allows for
+%   partial computations with parital save/load to the savefile filename.mat. This is an idea option
+%   for huge libraries that don't (or barely do) fit in memory --- the function will work with
+%   matfile() to dynamically read or write parts of the necessariy variables to disc. Instead of
+%   returning library, a matfile pointer, libfile, will be returned. At this point, typing
+%   libfile.library will load the library into memory. Since you probably had memory constraints,
+%   you may want to load the library in parts, just as libfile.library(1, :), etc.
 %
 %   Current Algorithm:
 %       Initiate by getting a first 'grid' of the top left index of every patch
@@ -51,14 +58,13 @@ function varargout = vol2lib(vol, patchSize, varargin)
         return
     end
     
-    if nargin == 2
-        varargin{1} = 'sliding';
-    end
+    % inputs
+    [patchOverlap, dofiledrop, dropfile] = parseInputs(varargin{:});
     nDims = ndims(vol);
     volSize = size(vol);
     
     % get the index and subscript of the initial grid
-    [initidx, cropVolSize, gridSize] = patchlib.grid(volSize, patchSize, varargin{:}); 
+    [initidx, cropVolSize, gridSize] = patchlib.grid(volSize, patchSize, patchOverlap{:}); 
     initsub = cell(1, nDims);
     [initsub{:}] = ind2sub(volSize, initidx);
     vol = cropVolume(vol, cropVolSize);
@@ -68,41 +74,78 @@ function varargout = vol2lib(vol, patchSize, varargin)
     [shift{:}] = ind2sub(patchSize, (1:prod(patchSize))');
     shift = [shift{:}];
     
-    % initialize library of subscripts into the volume
-    sub = cell(numel(patchSize), 1);
-    sub(:) = {zeros(numel(initidx), prod(patchSize), 'uint32')};
-    
-    % go through each shift
-    for s = 1:prod(patchSize)
-        
-        % update subscript library
-        for dim = 1:numel(patchSize)
-            sub{dim}(:, s) = initsub{dim}(:) + shift(s, dim) - 1;
-        end
+    % compute the actual library
+    if ~dofiledrop
+        library = memlib(vol, patchSize, cropVolSize, initsub, shift);
+        libsize = size(library);
+        outlib = library;
+    else
+        filelib(vol, patchSize, cropVolSize, initsub, shift, dropfile);
+        dropfile.initidx = initidx(:);
+        dropfile.cropVolSize = cropVolSize;
+        dropfile.gridSize = gridSize;
+        libsize = size(dropfile, 'library');
+        outlib = dropfile;
     end
     
-    % for each dimension, put the subscript library in a vector
-    sub = cellfun(@(x) x(:), sub, 'UniformOutput', false); 
+    % check final library size
+    assert(numel(initidx) == libsize(1), ...
+        'Something went wrong with the library of index computation. Sizes don''t match.');
+    assert(prod(gridSize) == libsize(1), ...
+        'Something went wrong with the library of index computation. Sizes don''t match.');
+    
+    % outputs 
+    outputs = {outlib, initidx(:), cropVolSize, gridSize};
+    varargout = outputs(1:nargout); 
+end
+
+function library = memlib(vol, patchSize, cropVolSize, initsub, shift)
+
+    % initialize library of subscripts into the volume
+    nDims = ndims(vol);
+    nGridVoxels = numel(initsub{1});
+    sub = cell(nDims, 1);
+    sub(:) = {zeros(nGridVoxels, prod(patchSize), 'uint32')};
+   
+    % update subscript library
+    for dim = 1:nDims
+        
+        % go through each shift
+        for s = 1:prod(patchSize)
+            sub{dim}(:, s) = initsub{dim}(:) + shift(s, dim) - 1;
+        end
+        
+        % put the subscript library in a vector
+        sub{dim} = sub{dim}(:);
+    end
     
     % compute the library of linear indexes into the volume
     idxvec = sub2ind(cropVolSize, sub{:});
     clear sub;
-    idx = reshape(idxvec, [numel(initidx), prod(patchSize)]);
-    
-    % compute final library
-    library = vol(idx(:));
-    library = reshape(library, size(idx));
 
-    % check final library size
-    assert(numel(initidx) == size(library, 1), ...
-        'Something went wrong with the library of index computation. Sizes don''t match.');
-    assert(prod(gridSize) == size(library, 1), ...
-        'Something went wrong with the library of index computation. Sizes don''t match.');
-    
-    % outputs
-    outputs = {library, initidx(:), cropVolSize, gridSize};
-    varargout = outputs(1:nargout); 
+    % compute final library
+    library = vol(idxvec(:));
+    library = reshape(library, [nGridVoxels, prod(patchSize)]);
 end
+
+
+function filelib(vol, patchSize, cropVolSize, initsub, shift, dropfile)
+
+    nGridVoxels = numel(initsub{1});
+    rows = ceil(numel(vol) ./ prod(patchSize));
+    dropfile.library(nGridVoxels, 1:prod(patchSize)) = 0; % pre-allocation inside matlab file
+    for i = 1:rows:nGridVoxels
+        range = i:min(i+rows-1, nGridVoxels);
+        tmpsub = cellfunc(@(x) x(range), initsub);
+        library = memlib(vol, patchSize, cropVolSize, tmpsub, shift);
+        
+        % todo specify cols?
+        dropfile.library(range, :) = library;
+    end
+end
+
+
+
 
 function varargout = vol2libcell(vol, patchSize, varargin)
 
@@ -129,3 +172,18 @@ function varargout = vol2libcell(vol, patchSize, varargin)
         varargout{5} = idxcell;
     end
 end        
+
+function [patchOverlap, dofiledrop, dropfile] = parseInputs(varargin)
+
+    patchOverlap = {'sliding'};
+    dofiledrop = false;
+    dropfile = [];
+    if nargin == 1 || nargin == 3
+        patchOverlap = varargin(1);
+    end
+    if nargin == 2 || nargin == 3
+        assert(strcmp(varargin{end-1}, 'savefile'));
+        dofiledrop = true;
+        dropfile = matfile(varargin{end}, 'Writable', true);
+    end
+end
