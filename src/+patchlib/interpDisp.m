b6ba2fed69fdd75333010b3beddb1fb66e6ef13f
Adrian Dalca <adalca@mit.edu>
2015-05-28 14:25:29 -0400

    fix bug in interpDisp

--- src/+patchlib/interpDisp.m
+++ src/+patchlib/interpDisp.m
@@ -1,14 +1,13 @@
-function disp2 = interpDisp(disp, patchSize, patchOverlap)
+function disp2 = interpDisp(disp, patchSize, patchOverlap, volSize)
 %INTERPDISP interpolate displacements given on a grid
-%   disp2 = interpDisp(disp, patchSize, patchOverlap)
+%   disp2 = interpDisp(disp, patchSize, patchOverlap, volSize)
 %
 %   TODO: allow for 
 %   disp2 = interpDisp(disp, idxsub)
 
-    volSize = size(disp{1});
-
     % get subscripts
     [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, 'sub');
+    assert(all(size(idxsub{1}) == size(disp{1})));
     assert(all(size(idxsub{1}) == gridsize));
     
     % get 
