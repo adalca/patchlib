6039457056e7b3dd4003d00c5567f9de0a4462b8
adalca <adalca@mit.edu>
2015-06-02 13:31:44 -0400

    adding shift to interpDisp

--- src/+patchlib/interpDisp.m
+++ src/+patchlib/interpDisp.m
@@ -1,16 +1,22 @@
-function disp2 = interpDisp(disp, patchSize, patchOverlap, volSize)
+function disp2 = interpDisp(disp, patchSize, patchOverlap, volSize, shift)
 %INTERPDISP interpolate displacements given on a grid
 %   disp2 = interpDisp(disp, patchSize, patchOverlap, volSize)
+%   disp2 = interpDisp(disp, patchSize, patchOverlap, volSize, shift)
 %
 %   TODO: allow for 
 %   disp2 = interpDisp(disp, idxsub)
 
     % get subscripts
-    [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, 'sub');
+    if exist('shift', 'var')
+        [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, shift, 'sub');
+    else
+        [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, 'sub');
+    end
     assert(all(size(idxsub{1}) == size(disp{1})));
     assert(all(size(idxsub{1}) == gridsize));
     
-    % get 
+    
+    % get the interpolated displacements. 
     xi = size2ndgrid(volSize);
     disp2 = cellfunc(@(x) interpn(idxsub{:}, x, xi{:}), disp);
     
