750699e14ed3d06d40790db1a6ca1d11e451116b
adalca <adalca@mit.edu>
2015-06-21 19:32:35 -0400

    fixing shift issue.

--- src/+patchlib/interpDisp.m
+++ src/+patchlib/interpDisp.m
@@ -1,21 +1,22 @@
-function disp2 = interpDisp(disp, patchSize, patchOverlap, volSize, startDel)
+function disp2 = interpDisp(disp, patchSize, patchOverlap, volSize, shift)
 %INTERPDISP interpolate displacements given on a grid
 %   disp2 = interpDisp(disp, patchSize, patchOverlap, volSize)
-%   disp2 = interpDisp(disp, patchSize, patchOverlap, volSize, startDel) startDel should be
-%       (patchsize + 1) / 2 in most cases, e.g. to get the centers
+%   disp2 = interpDisp(disp, patchSize, patchOverlap, volSize, shift) startdelta should be
+%       (patchsize - 1) / 2 in most cases, e.g. shift to get the centers
 %
 %   TODO: allow for 
 %   disp2 = interpDisp(disp, idxsub)
 
     % get subscripts
-    if exist('startDel', 'var')
-        [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, startDel, 'sub');
-    else
-        [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, 'sub');
+    [idxsub, ~, gridsize] = patchlib.grid(volSize, patchSize, patchOverlap, 'sub');
+    
+    if nargin <= 4
+        shift = volSize(:) * 0;
     end
+    
+    idxsub = cellfunc(@(x, y) x + y, idxsub, mat2cellsplit(shift(:)));
     assert(all(size(idxsub{1}) == size(disp{1})));
     assert((prod(gridsize) == 1 && numel(idxsub{1})) || all(size(idxsub{1}) == gridsize));
-    
     
     % get the interpolated displacements. 
     xi = size2ndgrid(volSize);
