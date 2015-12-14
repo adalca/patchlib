a86eedc270d439d9e8adb89cab9753008ff1d950
adalca <adalca@mit.edu>
2015-12-14 15:05:56 -0500

    typo

--- src/+patchlib/correspdst.m
+++ src/+patchlib/correspdst.m
@@ -22,7 +22,7 @@ function dst = correspdst(pstr1, pstr2, ~, ~, dvFact, usemex)
     Y = pstr2.disp ./ dvFact;
     
     % assumes patches is Nx2 where the second is [x, y]
-    % Note: sinec we call pdist2 many times with relatively few samples, this takes extra time since
+    % Note: since we call pdist2 many times with relatively few samples, this takes extra time since
     % pdist2 does quite a bit of argument checking before calling pdist2mex. pdist2mex is a built-in
     % private function in the statistics toolbox. To make this part much faster than pdist2(),
     % copy the pdist2mex file somewhere on your path. Note, however, that having an if statement
