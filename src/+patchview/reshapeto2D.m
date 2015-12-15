d30a4952a62c2db315d4feb038ecd113c85f49d8
adalca <adalca@mit.edu>
2015-12-15 18:36:36 -0500

    adding reshapeto2D

---
+++ src/+patchview/reshapeto2D.m
@@ -0,0 +1,29 @@
+function reshapedPatches = reshapeto2D(patches, patchSize)
+% reshapeto2D reshape ND patches to tiled 2D slices
+%
+%   reshapedPatches = reshapeto2D(alreadyShapedPatch)
+%
+%   reshapedPatches = reshapeto2D(patches, patchSize) assumes patches as in N*(prod(patchSize)) shape
+%
+% contact: adalca @ csail
+
+    if nargin == 1
+        patchSize = size(patches);
+        reshapedPatches = reshape(patches(:), [patchSize(1), prod(patchSize(2:end)]);
+
+    else
+        % split into a cell of patches
+        patchescell = dimsplit(1, patches);
+
+        % reshape each patch
+        pfn = @(p) patchview.reshapeto2D(reshape(p, patchSize), patchSize);
+        patchesreshapedcell = cellfunc(pfn, patchescell);
+
+        % add a line between patches.
+        % TODO: make this an option.
+        patchesreshapedcellline = cellfunc(@(x) [x; zeros(1, size(x, 2))], patchesreshapedcell);
+
+        % combine along first dimention
+        reshapedPatches = cat(1, patchesreshapedcellline{:});
+    end
+end
